2810eabe413c801f3a730bc7afa54dedacd2410c
Daniel Oselu <doselu3@gmail.com>
2020-12-17 22:26:12 +0300

    implement cases url-link isn't found

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,6 +9,15 @@ def index(request):
     })
 
 def wiki(request, name):
-    return render(request, "encyclopedia/entry.html", {
-        "name":util.get_entry(name)
-    })
+    if util.get_entry(name):
+        return render(request, "encyclopedia/entry.html", {
+            "name":util.get_entry(name),
+            "title":name
+        })
+    else:
+        error = "PAGE NOT FOUND"
+        error_code = "404"
+        return render(request, "encyclopedia/errors.html", {
+            "error":error,
+            "error_code":error_code
+        })
